a4052cc17b8cfb5349f84e7cc6edb453585d2166
Kartikeya <157278252+KartikeyaNainkhwal@users.noreply.github.com>
2026-04-02 05:44:24 +0530

    Update test-issue.ts

--- src/test-issue.ts
+++ src/test-issue.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export function login(user: string, pass: string) { 
   const db: any = {};
   const query = `SELECT * FROM users WHERE username = '${user}' AND password = '${pass}'`; 
